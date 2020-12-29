d26ecb8b3cdfd2f0e65a02780af41e90c3129a0e
Aaron Muesing <muesing.music@gmail.com>
2020-12-29 21:58:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,8 +130,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-if programmer_hash[:dennis_ritchie][:languages]["Assembly"] 
-  programmer_hash[:dennis_ritchie][:languages]["Assembly"] += 1
+if programmer_hash[:dennis_ritchie][:languages]["Assembly"] += 1
 programmer_hash
 
 end
